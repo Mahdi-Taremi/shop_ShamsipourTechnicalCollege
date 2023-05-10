55087a46d22da03af8a1500b1d58b1f892c56104
Your Name <sajjadtaremi99@gmail.com>
2023-05-10 22:36:16 +0330

    Try Show Data in Tabel

--- Controllers/ShopController.cs
+++ Controllers/ShopController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using shop_MahdiTaremi.Models;
 
 namespace shop_MahdiTaremi.Controllers
@@ -10,81 +11,103 @@ namespace shop_MahdiTaremi.Controllers
         {
             this.dbShop = dbShop;
         }
+        [HttpGet]
         public IActionResult Index()
         {
-            //var query = dbShop.Product.ToList();
-            
-            // List<Product> products = new List<Product>();  T
-
-
-            //Product objects = new Product();  T
-            //objects.Id = 1;
-            //objects.Name = "Name";
-            //objects.Price = 20;  
-            //objects.Quantity = 1;  
-            //objects.Color = "black";
-            //objects.Description = "Test 1";
-            //products.Add(objects);
-
-            //objects = new Product();  T
-            //objects.Id = 2;
-            //objects.Name = "Name2";
-            //objects.Price = 22;
-            //objects.Quantity = 2;
-            //objects.Color = "green";
-            //objects.Description = "Test 2";
-            //products.Add(objects);
-
-            //objects = new Product();   T
-            //objects.Id = 3;
-            //objects.Name = "Name3";
-            //objects.Price = 23;
-            //objects.Quantity = 3;
-            //objects.Color = "blue";
-            //objects.Description = "Test 3";
-            //products.Add(objects);
-
-            //var query = products.ToList();  
-            // var query = dbShop.Product.ToList();    TT
-            // var querySelect = products.ToList().Select(items => new {
-            //var querySelect = products.Select(items => new {   T
-            //items.Id,
-            //items.Name,
-            //items.Price,
-            //items.Quantity,
-            //items.Color,
-            //items.Description
-            //}).ToList(); 
-            // var querySelect =query.Select(items => new {
-            //    items.Id,
-            //    items.Name,
-            //    items.Price,
-            //     items.Quantity,
-            //    items.Color,
-            //    items.Description 
-            // });
-            // var query = products.Where(x => x.Name == "Name2").ToList();
-            // var query = products.Where(x => x.Id == 3).SingleOrDefault();
-            // var query = products.Where(x => x.Id == 3).FirstOrDefault();
-            // var query = products.Where(x => x.Id == 3).LastOrDefault();
-            // var query = products.Where(x => x.Id == 3).ToList();
-            // var query = products.Where(x => x.Name == "Name2").Any();
-            // var query = products.Where(x => x.Name == "Name2").Count();
-            // var query = products.Where(x => x.Name.Contains("Name2")).ToList();
-
-            // return View(query);  T
-            return View();
+            return View();  
         }
+        //public IActionResult Index()
+        //{
+        //var query = await dbShop.Product.ToListAsync();
+        //var query = dbShop.Product.ToList();
+        //List<Product> query = dbShop.Product.ToList();
+        //List<Product> query = new List<Product>();
+        //List<Product> query = dbShop.Product.ToList();
+
+
+        //Product objects = new Product();  T
+        //objects.Id = 1;
+        //objects.Name = "Name";
+        //objects.Price = 20;  
+        //objects.Quantity = 1;  
+        //objects.Color = "black";
+        //objects.Description = "Test 1";
+        //products.Add(objects);
+
+        //objects = new Product();  T
+        //objects.Id = 2;
+        //objects.Name = "Name2";
+        //objects.Price = 22;
+        //objects.Quantity = 2;
+        //objects.Color = "green";
+        //objects.Description = "Test 2";
+        //products.Add(objects);
+
+        //objects = new Product();   T
+        //objects.Id = 3;
+        //objects.Name = "Name3";
+        //objects.Price = 23;
+        //objects.Quantity = 3;
+        //objects.Color = "blue";
+        //objects.Description = "Test 3";
+        //products.Add(objects);
+
+        //var query = products.ToList();  
+        //var query = dbShop.Product.ToList();   
+        // var querySelect = products.ToList().Select(items => new {
+        //var querySelect = products.Select(items => new {   T
+        //items.Id,
+        //items.Name,
+        //items.Price,
+        //items.Quantity,
+        //items.Color,
+        //items.Description
+        //}).ToList(); 
+        // var querySelect =query.Select(items => new {
+        //    items.Id,
+        //    items.Name,
+        //    items.Price,
+        //     items.Quantity,
+        //    items.Color,
+        //    items.Description 
+        // });
+        // var query = products.Where(x => x.Name == "Name2").ToList();
+        // var query = products.Where(x => x.Id == 3).SingleOrDefault();
+        // var query = products.Where(x => x.Id == 3).FirstOrDefault();
+        // var query = products.Where(x => x.Id == 3).LastOrDefault();
+        // var query = products.Where(x => x.Id == 3).ToList();
+        // var query = products.Where(x => x.Name == "Name2").Any();
+        // var query = products.Where(x => x.Name == "Name2").Count();
+        // var query = products.Where(x => x.Name.Contains("Name2")).ToList();
+
+        // return View(query);  T
+        //  return View();
+        // }
         public IActionResult BuyShop()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult BuyShopAction(Product f)
+        public async Task<IActionResult> BuyShopAction(Product f)
         {
+            if (ModelState.IsValid)
+            {
             dbShop.Product.Add(f);
-            dbShop.SaveChanges();                
-            return RedirectToAction("Index");
+            await dbShop.SaveChangesAsync();                
+
+            return RedirectToAction("Details", new {id =f.Id});
+            //return RedirectToAction("Details",new {id =f.Id});
+            }
+            return View("Index");
+        }
+        public async Task<IActionResult> Details(int id)
+        {
+            var product = await dbShop.Product.FindAsync(id);   
+            if (product == null)
+            {
+                return View("Error");
+            }
+            return View(product);
         }
 
     }
--- Models/dbShop.cs
+++ Models/dbShop.cs
@@ -1,13 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace shop_MahdiTaremi.Models
 {
     public class dbShop:DbContext
     {
         //ctor
-        public dbShop(DbContextOptions options):base(options) { 
+        public dbShop(DbContextOptions<dbShop> options):base(options) { 
             
         }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<dbShop>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("Server=.;Database=shopShamsipourTechnicalCollege;User Id=MahdiTaremi;Password=12;TrustServerCertificate=True;")));
+
+            services.AddControllersWithViews();
+        }
+
         //DbSet is Key Microsoft And Table DB with Models
         public DbSet<Product> Product { get; set; }
     }
